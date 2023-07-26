3d42bc494f94876c51e2439218f834af507e6a5d
MuhammadUmarAtiq <umaratiq5555@gmail.com>
2023-07-26 19:59:21 +0500

    second

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
-import { Route, Routes ,Switch } from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
 const App=()=>{
   return<>
-    {/* <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/project" element={<Project/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes> */}
     <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
